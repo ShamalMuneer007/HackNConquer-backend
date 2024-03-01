b882e707a75eb0f4ac2c845af243978551c3b571
Shamal Muneer <shamal20017@gmail.com>
2024-03-01 12:18:50 +0530

    feat: add problem service

---
+++ problem-service/src/main/java/org/hackncrypt/problemservice/controllers/admin/AdminProblemController.java
@@ -0,0 +1,11 @@
+package org.hackncrypt.problemservice.controllers.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@Slf4j
+public class AdminProblemController {
+}
